6ea6a46815c7ec0966e086c2df86168461b442de
krish80552 <94895903+krish80552@users.noreply.github.com>
2022-01-21 01:33:17 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,32 +1,34 @@
 resource "aws_instance" "web" {
-  ami           = "ami-00f7e5c52c0f43726"
+  ami           = "ami-066333d9c572b0680"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0785ae796b2761b5a"]
   key_name = "Krish" 
-  
-  data "local_file" "private_key" {
-    filename = "${(root/Krish)}"
-}
-  
+         
+          
    connection {
     host        = "${self.public_ip}"
     user        = "ec2-user"
-    type        = "ssh"
-    private_key = data.local_file.private_key
+    type        = "ssh" 
+    private_key =  "${file("/home/ec2-user/Krish.pem")}"
   }
   
+provisioner "file" {
+    source      = "/home/ec2-user/Krish.pem"
+    destination = "/home/ec2-user/Krish.pem"
+  } 
+    
    provisioner "remote-exec" {
     inline = [
-      "chmod 400 /home/ec2-user/Krish",
+      "chmod 400 /home/ec2-user/Krish.pem",
       "sudo amazon-linux-extras install java-openjdk11 -y",
       "sudo yum install java-1.8.0-openjdk -y",
     ]
   }
 
-  provisioner "local-exec" {
+ provisioner "local-exec" {
     command = "echo ${self.public_ip} ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/root/Krish > /etc/ansible/hosts"
-  }
-  
+
+
 
   tags = {
     Name = "HelloWorld"
