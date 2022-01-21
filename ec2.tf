4843e7129593225bccf3e6ba9d3ee28b9d70e390
krish80552 <94895903+krish80552@users.noreply.github.com>
2022-01-21 09:36:16 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,8 +8,10 @@ resource "aws_instance" "web" {
     host        = "${self.public_ip}"
     user        = "ec2-user"
     type        = "ssh" 
-    private_key =  "${file("/home/ec2-user/Krish.pem")}"
+    private_key = "${file("/home/ec2-user/Krish.pem")}"
   }
+  
+  
   
     
    provisioner "remote-exec" {
