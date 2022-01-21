383fa92dbfc03d9180c01b8d9d948c16496685ae
krish80552 <94895903+krish80552@users.noreply.github.com>
2022-01-21 23:31:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,52 +1,39 @@
 resource "aws_instance" "web" {
-  ami           = "ami-066333d9c572b0680"
-  instance_type = "t3.micro"
+  ami                    = "ami-066333d9c572b0680"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0785ae796b2761b5a"]
-  key_name = "Krish" 
-  
+  key_name               = "Krish"
 
 
-#  connection {
-#    host = "${self.public_ip}"
-#    user = "ec2-user"
-#    type = "ssh"
-#    private_key = "resource.aws_insatnce.web.key_name"
-#  }
-  
-#  provisioner "file" {
-#    source = "/home/ec2-user/Krish.pem"
-#    destination = "/home/ec2-user/Krish.pem"
-#  }
-  
-  
-  
-    
-   provisioner "remote-exec" {
-    inline = [
-      "sudo amazon-linux-extras install java-openjdk11 -y",
-      "sudo yum install java-1.8.0-openjdk -y",
-    ]
-   }
-   connection {
-       type        = "ssh"
-       host        = self.public_ip
-       user        = "ec2-user"
-       private_key = file("/home/ec2-user/Krish.pem")
-       timeout     = "4m"
-   }
-  }
+connection {
+  host        = self.public_ip
+  user        = "ec2-user"
+  type        = "ssh"
+  private_key = file("/home/ec2-user/Krish.pem")
+}
 
- provisioner "local-exec" {
-    command = "echo ${self.public_ip} ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/root/Krish > /etc/ansible/hosts"
+provisioner "file" {
+  source      = "/home/ec2-user/Krish.pem"
+  destination = "/home/ec2-user/Krish.pem"
+}
 
- }
+provisioner "remote-exec" {
+  inline = [
+    "chmod 400 /home/ec2-user/Krish.pem",
+    "sudo amazon-linux-extras install java-openjdk11 -y",
+    "sudo yum install java-1.8.0-openjdk -y",
+  ]
+}
 
-  tags = {
-    Name = "HelloWorld"
-  }
-  
+provisioner "local-exec" {
+  command = "echo ${self.public_ip} ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/root/Krish > /etc/ansible/hosts"
+
 }
 
 
-  
+tags = {
+  Name = "HelloWorld"
+}
 
+
+}
