5aed1abbd562fbbb8c0f7e9fb2f995a64abf0ed1
krish80552 <94895903+krish80552@users.noreply.github.com>
2022-01-22 00:21:37 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -26,7 +26,7 @@ provisioner "remote-exec" {
 }
 
 provisioner "local-exec" {
-  command = "echo ${self.public_ip} ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/home/ec2-user/Krish > /etc/ansible/hosts"
+  command = "echo ${self.public_ip} ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/home/ec2-user/Krish.pem > /etc/ansible/hosts"
 
 }
 
