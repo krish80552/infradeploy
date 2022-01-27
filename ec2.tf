3c47288105ec882e87e18139ec2142a6c57597d6
krish80552 <94895903+krish80552@users.noreply.github.com>
2022-01-26 23:59:57 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -22,14 +22,11 @@ provisioner "remote-exec" {
     "chmod 400 /home/ec2-user/Krish.pem",
     "sudo amazon-linux-extras install java-openjdk11 -y",
     "sudo yum install java-1.8.0-openjdk -y",
-    "cp Krish.pem /tmp/Krish.pem",
-    "chown jenkins:jenkins /tmp/Krish.pem",
-    "chown 400 /tmp/Krish.pem",
   ]
 }
 
 provisioner "local-exec" {
-  command = "echo ${self.public_ip} ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/tmp/Krish.pem > /tmp/hosts"
+  command = "echo ${self.public_ip} ansible_ssh_user=ec2-user ansible_ssh_private_key_file=Krish.pem > /tmp/hosts"
 
 }
 
